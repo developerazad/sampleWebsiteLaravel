c252437c8b230a2afb919cdd4256a1493edb1f25
developerazad <developerazad.hstu@gmail.com>
2019-06-16 22:04:53 +0600

    created table using migration,add code in appServiceProvider, insert data and return success message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -11,6 +12,19 @@ class MessagesController extends Controller
             'name'  => 'required',
             'email' => 'required'
         ]);
-        return 'SUCCESS';
+        //return 'Success';
+        //create New Message
+        $message = new Message();
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+
+        // Save Message
+        $message->save();
+
+        // Redirect
+        return redirect('/')->with('success','Message Sent');
+
+
     }
 }
--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,7 +1,15 @@
-@if(count($errors) > 0)
-    @foreach($errors->all() as $error)
-        <div class="alert alert-danger">
-            {{$error}}
+    {{--show if there are errors--}}
+    @if(count($errors) > 0)
+        @foreach($errors->all() as $error)
+            <div class="alert alert-danger">
+                {{$error}}
+            </div>
+        @endforeach
+    @endif
+
+    {{--show success message--}}
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
-    @endforeach
-@endif
+    @endif
